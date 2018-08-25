afe966571c6cc981ccf2b61991e6aef117d5b11f
Angelmario Santana <angelmario.santana@live.com>
2018-08-25 14:27:45 -0300

    Iniciando API

--- config/index.js
+++ config/index.js
@@ -1,4 +1,15 @@
-module.exports = {
-  port: process.env.PORT,
-  database: process.env.DATABASE,
-}
+// module.exports = {
+//   port: process.env.PORT,
+//   database: process.env.DATABASE,
+// }
+
+
+var nodeEnv = process.env.NODE_ENV !== undefined ? process.env.NODE_ENV : 'production';
+
+var config = {
+    development: require('./development'),
+    production: require('./production'),
+    test: require('./test')
+};
+
+module.exports = config[nodeEnv];
