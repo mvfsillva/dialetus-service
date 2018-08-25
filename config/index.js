b629b4b6b711e51fe9edd493b09b067d0fd1bd71
Angelmario Santana <angelmario.santana@live.com>
2018-08-25 18:19:38 -0300

    Fix update files

--- config/index.js
+++ config/index.js
@@ -1,15 +1,4 @@
-// module.exports = {
-//   port: process.env.PORT,
-//   database: process.env.DATABASE,
-// }
-
-
-var nodeEnv = process.env.NODE_ENV !== undefined ? process.env.NODE_ENV : 'production';
-
-var config = {
-    development: require('./development'),
-    production: require('./production'),
-    test: require('./test')
-};
-
-module.exports = config[nodeEnv];
+module.exports = {
+	port: process.env.PORT,
+	database: process.env.DATABASE
+}
--- server.js
+++ server.js
@@ -1,35 +1,27 @@
-'use strict'
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
-const mongoose = require('mongoose')
 
 require('dotenv').load()
 
 const config = require('./config')
-const connectDB = require('./lib/helpers/db')
 const routes = require('./lib/routes')
 const logger = require('./logger')
 
 const app = express()
 
-mongoose.Promise = global.Promise
-
-connectDB()
-
 app.config = config
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(expressValidator())
-app.set('port', config.port)
+app.set('port', 4000)
 routes(app)
 
-app.listen(config.port, () => {
+app.listen(4000, () => {
   logger.info(
     `Dialetus API server is listening on port ${app.get(
       'port'
-    )} (http://localhost:${config.port})`
+    )} (http://localhost:4000)`
   )
 })
 
