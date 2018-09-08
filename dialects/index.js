5d783e78969b06986584df8ad94e29ee8f96e579
Diego Ramos <rdiego26@gmail.com>
2018-09-08 20:58:20 -0300

    add some carioca dialects (#7)
    * fix ignore package-lock.json
    
    * add some carioca sentences.
    
    * add require for carioques.json

--- dialects/index.js
+++ dialects/index.js
@@ -4,4 +4,5 @@ module.exports = Object.freeze({
   baianes: require('./baianes.json'),
   mineires: require('./mineires.json'),
   paranes: require('./paranes.json'),
+  carioques: require('./carioques.json'),
 })
