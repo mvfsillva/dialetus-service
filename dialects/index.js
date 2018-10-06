cb90a82d3a566c8bc25031b3eeb5c15b8cc9c8e5
Rodolfo Silva <contato@rodolfosilva.com>
2018-10-06 10:13:43 -0300

    Add slug and enpoint to retive a dialect

--- dialects/index.js
+++ dialects/index.js
@@ -2,6 +2,7 @@
 
 const path = require('path')
 const glob = require('glob')
+const slugify = require('slugify')
 
 const loadFile = extension => filePath => [
   path.basename(filePath, extension),
@@ -9,12 +10,23 @@ const loadFile = extension => filePath => [
 ]
 const zip = (object, [key, value]) => ({ ...object, [key]: value })
 
+const addSlugToDialect = dialect => ({
+  slug: slugify(dialect.dialect, { lower: true, remove: /[*+~.?()'"!:@]/g }),
+  ...dialect,
+})
+
+const slugifyAllRegions = ([region, dialects]) => [
+  region,
+  dialects.map(addSlugToDialect),
+]
+
 const PATTERN_DIALECTS = path.join(__dirname, '/*.json')
 
 const loadDialects = () =>
   glob
     .sync(PATTERN_DIALECTS)
     .map(loadFile('.json'))
+    .map(slugifyAllRegions)
     .reduce(zip, {})
 
 module.exports = Object.freeze({
