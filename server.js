88a8cb5342a8fbdd4c3530be98400cf3f39f2e24
Will Mendes <willmendesneto@gmail.com>
2018-10-08 14:49:01 +1100

    test: adding test automation (#54)
    * test: adding unit/integration tests
    
    * chore: removing unused folder
    
    * chore: removing .eslint file
    
    * chore(test): improving test describe text

--- server.js
+++ server.js
@@ -1,24 +1,6 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const expressValidator = require('express-validator')
-const cors = require('cors')
-
-require('dotenv').load()
-
+const app = require('./app')
 const config = require('./config')
-const routes = require('./lib/routes')
 const logger = require('./logger')
-
-const app = express()
-
-app.config = config
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(expressValidator())
-app.set('port', config.port)
-
-app.use(routes())
 
 app.listen(config.port, () => {
   logger.info(
---
+++ test/integration/regions.js
@@ -0,0 +1,60 @@
+const chai = require('chai')
+const app = require('../../app')
+
+describe('Integration: Dialect Regions endpoint', () => {
+  let server
+  const expectedResponse = [
+    {
+      name: 'baianes',
+      total: 27,
+    },
+    {
+      name: 'carioques',
+      total: 13,
+    },
+    {
+      name: 'catarines',
+      total: 8,
+    },
+    {
+      name: 'cearences',
+      total: 10,
+    },
+    {
+      name: 'mineires',
+      total: 21,
+    },
+    {
+      name: 'paranes',
+      total: 53,
+    },
+    {
+      name: 'pernambuques',
+      total: 12,
+    },
+    {
+      name: 'potiguares',
+      total: 30,
+    },
+    {
+      name: 'rondones',
+      total: 9,
+    },
+  ]
+
+  before(() => {
+    server = app.listen()
+  })
+
+  after(() => server.close())
+
+  it('GET `/regions` should return all available dialects', async () => {
+    const res = await chai
+      .request(server)
+      .get('/regions')
+      .redirects(0)
+
+    expect(res).to.have.status(200)
+    expect(res.body).to.eql(expectedResponse)
+  })
+})
