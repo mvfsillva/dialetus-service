096d0b6e43265c5a4d449753e7ea9393f84a2c9c
mvfsilva <mvfsillva@gmail.com>
2019-04-10 16:43:51 -0300

    refactor: change local logger to hoopa-logger

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
+const logger = require('hoopa-logger')
+
 const app = require('./app')
 const config = require('./config')
-const logger = require('./logger')
 
 app.listen(config.port, () => {
   const port = app.get('port')
-  logger.info(
-    `Dialetus API server is listening on port ${port} (http://localhost:${config.port})`
-  )
+  logger.info(`Dialetus API server is listening on port ${port} (http://localhost:${config.port})`)
 })
 
 module.exports = app
