a12ed326d6244674ffa769092d3c7a44329ac29e
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2021-02-25 20:57:33 -0300

    #319 - created a hello world function, configured the initial serverless yml and the scripts in the package.json

---
+++ handler.js
@@ -0,0 +1,15 @@
+async function helloHandler(event) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v1.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+}
+
+export { helloHandler };
