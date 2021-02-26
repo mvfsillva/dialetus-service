c659a1d418435c03d35476c5d08e7919f8dd976a
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2021-02-25 21:27:02 -0300

    #319 - fixed lint errors

--- src/handler.js
+++ src/handler.js
@@ -1,15 +1,4 @@
-var malquias = null;
-
 async function helloHandler(event) {
-  if (1) {
-    if (2) {
-      if (3) {
-        if (4) {
-          return 'amor';
-        }
-      }
-    }
-  }
   return {
     statusCode: 200,
     body: JSON.stringify(
