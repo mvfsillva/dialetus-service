f07be6e594930f07a7327777e820cc723699f364
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2021-02-28 18:45:57 -0300

    #319 - UF must be uppercase in region table

--- src/functions/create-new-region/use-case/index.js
+++ src/functions/create-new-region/use-case/index.js
@@ -2,7 +2,7 @@ async function createNewRegion(
   { uf = '', label = '', draft = 'true' },
   { dynamoDBSaveItem, tableName },
 ) {
-  return dynamoDBSaveItem(tableName, { uf: uf, label, draft });
+  return dynamoDBSaveItem(tableName, { uf: uf.toUpperCase(), label, draft });
 }
 
 export { createNewRegion };
