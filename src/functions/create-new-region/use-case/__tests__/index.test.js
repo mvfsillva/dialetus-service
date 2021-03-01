c4ec0862b9de556005453d508e71b5315df8572f
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2021-03-01 09:34:34 -0300

    #319 - create a unit test to validate that the code is not broken when we have a exception

--- src/functions/create-new-region/use-case/__tests__/index.test.js
+++ src/functions/create-new-region/use-case/__tests__/index.test.js
@@ -17,4 +17,27 @@ describe('# Functions - createNewRegion - use-case', () => {
     expect(typeof newRegion).toBe('object');
     expect(newRegion).toMatchObject({ label: 'Bahia', uf: 'BA', draft: true });
   });
+
+  it('## should return an error when try save new region because label is empty', async () => {
+    const dynamoDBSaveItem = async (tableName, data) => {
+      if (data && !data.label) {
+        const error = new Error('One or more parameter values are not valid.');
+        error.code = 'ValidationException';
+        error.statusCode = 400;
+        throw error;
+      }
+      return data;
+    };
+    const tableName = 'mocked-table-data';
+
+    const data = {
+      uf: 'ba',
+      draft: true,
+    };
+    try {
+      await createNewRegion(data, { dynamoDBSaveItem, tableName });
+    } catch (error) {
+      expect(typeof error).toBe('object');
+    }
+  });
 });
