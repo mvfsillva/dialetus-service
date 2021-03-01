8f6c79f8f9cd32d9a86263341d08605dd358eb0a
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2021-03-01 11:19:00 -0300

    #319 - fix findregions query must has draft as false by default

--- src/components/dynamodb/querys/find-regions.js
+++ src/components/dynamodb/querys/find-regions.js
@@ -4,7 +4,7 @@ import { convertRecordsToObject } from '../utils/convert-records-to-object';
 const dynamoDB = new AWS.DynamoDB();
 
 async function findRegions(tableName) {
-  const statement = `SELECT * FROM "${tableName}" WHERE "draft" = 'true'`;
+  const statement = `SELECT * FROM "${tableName}" WHERE "draft" = 'false'`;
   const { Items } = await dynamoDB.executeStatement({ Statement: statement }).promise();
   return convertRecordsToObject(Items);
 }
